104782cee973d055521a366914d47f72ac290a53
Francesca Driver <kef.driver@gmail.com>
2023-05-24 21:30:08 +0400

    added controller, view and route files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+puts 'Creating 5 fake restaurants...'
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: CATEGORIES.sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
